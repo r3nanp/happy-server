f21de3ce35fa5e138d367b0fe78f12956253182b
Renan Pereira <renanmol87@gmail.com>
2021-08-18 16:03:46 +0000

    feat(middlewares): create auth middleware

---
+++ src/infra/http/middlewares/ensureAuthenticated.ts
@@ -0,0 +1,37 @@
+import { Request, Response, NextFunction } from 'express'
+import jwt from 'jsonwebtoken'
+
+import { AppError } from '@errors/AppError'
+import { auth } from '@config/auth'
+
+interface ITokenPayload {
+  iat: number
+  exp: number
+  sub: string
+}
+
+export function ensureAuthenticated(
+  request: Request,
+  _response: Response,
+  next: NextFunction
+): void | Error {
+  const authHeader = request.headers.authorization
+
+  if (!authHeader) {
+    throw new AppError('JWT token is missing.')
+  }
+
+  const [, token] = authHeader.split(' ')
+
+  try {
+    const decodedToken = jwt.verify(token, auth.secret)
+
+    const { sub } = decodedToken as ITokenPayload
+
+    request.user_id = sub
+
+    return next()
+  } catch {
+    throw new AppError('Invalid JWT token', 501)
+  }
+}
