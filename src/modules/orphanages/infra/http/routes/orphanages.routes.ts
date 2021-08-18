57578ae8c05f41e8832177a8172d016f05159675
Renan Pereira <renanmol87@gmail.com>
2021-08-18 16:10:35 +0000

    feat: add middleware

--- src/modules/orphanages/infra/http/routes/orphanages.routes.ts
+++ src/modules/orphanages/infra/http/routes/orphanages.routes.ts
@@ -5,6 +5,8 @@ import { upload } from '@config/upload'
 import { createOrphanageController } from '../controllers/CreateOrphanage'
 import { searchOrphanageController } from '../controllers/SearchOrphanage'
 import { showOrphanageController } from '../controllers/ShowOrphanage'
+import { deleteOrphanageController } from '../controllers/DeleteOrphanage'
+import { ensureAuthenticated } from '@infra/http/middlewares/ensureAuthenticated'
 
 const orphanageRoutes = Router()
 const uploadConfig = multer(upload)
@@ -18,5 +20,8 @@ orphanageRoutes.post(
   uploadConfig.array('images'),
   createOrphanageController.create
 )
+
+orphanageRoutes.use(ensureAuthenticated)
+orphanageRoutes.delete('/:id', deleteOrphanageController.delete)
 
 export { orphanageRoutes }
