b7ca0cf9454587f6fbc6902dd8638ef1dd73ce88
Renan Pereira <renanmol87@gmail.com>
2021-08-18 16:09:59 +0000

    feat: add routes

--- src/infra/http/routes/app.routes.ts
+++ src/infra/http/routes/app.routes.ts
@@ -1,8 +1,14 @@
 import { Router } from 'express'
 import { orphanageRoutes } from '@modules/orphanages/infra/http/routes/orphanages.routes'
+import { usersRouter } from '@modules/users/infra/http/routes/users.routes'
+import { sessionsRouter } from '@modules/users/infra/http/routes/sessions.routes'
+import { passwordRouter } from '@modules/users/infra/http/routes/password.routes'
 
 const routes = Router()
 
 routes.use('/orphanages', orphanageRoutes)
+routes.use('/users', usersRouter)
+routes.use('/sessions', sessionsRouter)
+routes.use('/password', passwordRouter)
 
 export { routes }
